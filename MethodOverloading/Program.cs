9a38f59f64b5d6ac28941889daa1775105401c54
Chinedu Obinnah~ <cobinnah@yahoo.com>
2023-05-19 05:46:09 -0500

    Overloading complete

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var x = 10;
+            var y = 20;
+
+            var a = 10;
+            var b = 20;
+
+            var intAdd = Add(x, y);
+            var decimalAdd= Add(a, b);
+            var addwithBool = Add(0, 1, true);
+            Console.WriteLine($" Int Add: {x} + {y} = {intAdd}/nDecimal Add: {a} + {b} = {decimalAdd}/n Bool Add: {addwithBool}");
+
+           
         }
+        public static int Add(int num1, int num2)
+        {
+            return  num1 + num2 ;
+        }
+        public static decimal Add( decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool isChecked)
+        {
+            var addedNums = num1 + num2;
+            if (isChecked && addedNums > 1)
+            {
+                return $"{addedNums}";
+            }
+            else if (isChecked && addedNums == 1)
+            {
+                return $"{addedNums} dollar";
+            }
+
+            else
+            {
+                return addedNums.ToString();
+            }
+            }
+
+        
+
+
+         
+
     }
 }
